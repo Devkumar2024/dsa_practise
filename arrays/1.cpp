19c38726a799b11bd90419336560af637036f370
Devkumar2024 <kumarravi9158@gmail.com>
2024-05-28 11:06:58 +0530

    Initial commit

---
+++ arrays/1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[100], n;
+    cout <<"Enter size of array = ";
+    cin >> n;
+    cout <<"Enter elements of array = "<<endl;
+    for(int i = 0 ; i<n; i++ )
+    {
+        cin >> arr[i];
+    }
+
+    int ans = INT16_MAX;
+     for(int i = 0 ; i<n; i++ )
+     {
+        if(arr[i]<ans)
+        {
+            ans = arr[i];
+        }
+     }
+     cout <<"Minimum element of array = "<< ans;
+}
